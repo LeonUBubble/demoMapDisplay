c3e63302046f555667a621d996acd4ea3672b371
LeonUBubble <1043065350@qq.com>
2018-11-21 12:17:59 -0500

    配置pom.xml mybaits

--- src/main/java/com/example/demo/mapper/MapsMapper.java
+++ src/main/java/com/example/demo/mapper/MapsMapper.java
@@ -1,6 +1,10 @@
 package com.example.demo.mapper;
 import java.util.List;
-
+import com.example.demo.po.Maps;
 public interface MapsMapper {
+
+    public void updatMaps(Maps map) throws Exception;
+    public List<String> findMapsByUserid(int userid) throws Exception;
+    public String findLayerTreeByMaps(Maps map) throws Exception;
 
 }
